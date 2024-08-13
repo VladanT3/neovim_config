c3c1d021b92bdefbad5207837f29e6554309072d
VladanT3 <vladt.t33@gmail.com>
2024-08-13 17:14:21 +0200

    added tmux-navigator plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -63,6 +63,10 @@ require("lazy").setup({
 
 	-- Auto closes parenthesis, quotes, etc...
 	require("plugins.autoclose"),
+
+	-- Tmux navigator
+	require("plugins.tmux-navigator"),
+
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
