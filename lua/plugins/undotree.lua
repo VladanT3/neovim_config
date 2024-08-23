6b5e35c044fb38698f844fb100c68ff0e8230e1d
VladanT3 <vladt.t33@gmail.com>
2024-08-23 10:57:03 +0200

    added ignoring of node_modules to telescope, added gruvbox as a theme, changed desc for undotree keymap

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,4 +1,4 @@
 return {
 	"mbbill/undotree",
-	vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle),
+	vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle, { desc = "Toggle [U]ndo [T]ree" }),
 }
--- lua/theme.lua
+++ lua/theme.lua
@@ -4,14 +4,16 @@ return {
 		-- change the command in the config to whatever the name of that colorscheme is.
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"catppuccin/nvim",
-		name = "catppuccin",
+		"sainnhe/gruvbox-material",
+		lazy = false,
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("catppuccin-macchiato")
+			vim.g.gruvbox_material_background = "hard"
+			vim.g.gruvbox_material_foreground = "material"
+			vim.cmd.colorscheme("gruvbox-material")
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
