6c9e92dfaf5884f0b63fae70682f71d93a554e45
VladanT3 <vladt.t33@gmail.com>
2025-04-24 15:03:01 +0200

    conform changes

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -31,7 +31,7 @@ return {
 				c = { "indent" },
 				cpp = { "indent" },
 				--java = { "google_java_format" },
-				python = { "black" },
+				python = { "ruff_format" },
 				-- Conform can also run multiple formatters sequentially
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
