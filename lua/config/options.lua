c54d076128425e3f59057ef3731beced40a5df92
VladanT3 <vladt.t33@gmail.com>
2025-04-27 13:21:36 +0200

    removed osc 52 stuff its too broken

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,24 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
--- osc52 extra configs
-vim.g.clipboard_osc52_silent = false -- Set to `true` to suppress messages
-vim.g.clipboard_osc52_timeout = 1000 -- Timeout in milliseconds (abort if no response)
-
--- Enable OSC 52 clipboard support
-vim.g.clipboard = {
-  name = "osc52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
-
 local opt = vim.opt
 
 opt.relativenumber = true
